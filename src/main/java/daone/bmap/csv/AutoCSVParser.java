19961771ea466c112284a1e906965b71d9b2f2f6
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-24 15:29:43 +0900

    Park API 개발 #1 - 주차장 데이터 00시 마다 업데이트 자동화

---
+++ src/main/java/daone/bmap/csv/AutoCSVParser.java
@@ -0,0 +1,33 @@
+package daone.bmap.csv;
+
+import daone.bmap.service.ParkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@RequiredArgsConstructor
+@Component
+public class AutoCSVParser {
+
+    private final ParkService parkService;
+
+    @Scheduled(cron = "0 0 0 * * *")        //cron = 초 분 시 일 월 요일
+    public void parse() {
+
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process p1 = runtime.exec("cmd /c start D:\\project\\ParkingService\\src\\main\\java\\jh\\ParkingService\\csv\\autoCsvInstall.bat");
+            InputStream is = p1.getInputStream();
+            int i = 0;
+            while ((i = is.read()) != -1) {
+                System.out.print((char) i);
+            }
+            parkService.saveParkData();
+        } catch (IOException ioException) {
+            System.out.println(ioException.getMessage());
+        }
+    }
+}
