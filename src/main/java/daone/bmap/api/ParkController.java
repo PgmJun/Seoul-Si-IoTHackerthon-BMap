942e196cb870f38abbcbe96bd862014717e4ddac
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-24 15:29:43 +0900

    Park API 개발 #1 - 주차장 데이터 00시 마다 업데이트 자동화

--- src/main/java/daone/bmap/api/ParkController.java
+++ src/main/java/daone/bmap/api/ParkController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +18,17 @@ import java.util.List;
 @RestController
 public class ParkController {
     private final ParkService parkService;
+
+    @PutMapping
+    public ResponseEntity<String> patchParkingData(){
+        try {
+            parkService.saveParkData();
+            return ResponseEntity.ok("주차장 데이터 패치 완료");
+        } catch (Exception e){
+            log.error("::ERROR:: ParkController.java -> patchParkingData");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
     @GetMapping("/all")
     public ResponseEntity<List<ParkDto>> findAllParkingData(){
