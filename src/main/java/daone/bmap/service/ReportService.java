ec465882610ec4d16a6c5b730891fe9c75367714
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-24 10:31:50 +0900

    Report CRUD 개발 #2 Report Repository, Service 개발

---
+++ src/main/java/daone/bmap/service/ReportService.java
@@ -0,0 +1,32 @@
+package daone.bmap.service;
+
+
+import daone.bmap.domain.report.Report;
+import daone.bmap.domain.report.ReportRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ReportService {
+    private final ReportRepository reportRepository;
+
+
+//    Report save(Report report){
+//
+//    }
+//
+//    List<Report> findReportByreportId(Long reportId){
+//
+//    }
+//
+//    List<Report> findAll(){
+//
+//    }
+
+}
