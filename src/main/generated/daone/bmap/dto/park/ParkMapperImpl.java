74035cd2a5db4db50974dad46f0d940cc027ce04
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-25 01:06:34 +0900

    Report API 개발 #2 saveReport API 오류 해결해야함

--- src/main/generated/daone/bmap/dto/park/ParkMapperImpl.java
+++ src/main/generated/daone/bmap/dto/park/ParkMapperImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-08-25T00:21:44+0900",
+    date = "2022-08-25T00:37:55+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.11 (Oracle Corporation)"
 )
 public class ParkMapperImpl implements ParkMapper {
--- src/main/java/daone/bmap/api/ReportController.java
+++ src/main/java/daone/bmap/api/ReportController.java
@@ -1,6 +1,7 @@
 package daone.bmap.api;
 
 import daone.bmap.domain.report.Report;
+import daone.bmap.domain.report.ReportType;
 import daone.bmap.dto.report.ReportMapper;
 import daone.bmap.dto.report.ReportRequestDto;
 import daone.bmap.service.ReportService;
@@ -8,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportController {
     private final ReportService reportService;
 
-    @PostMapping
-    public ResponseEntity<String> saveReport(ReportRequestDto requestDto){
+    @PostMapping("/add")
+    public ResponseEntity<String> saveReport(@RequestBody ReportRequestDto data){
         try {
-            Report report = ReportMapper.mapper.reportRequestDtoToEntity(requestDto);
+            ReportType reportType;
+            if (data.getReportType() == 0) reportType = ReportType.ERROR;
+            else if (data.getReportType() == 1) reportType = ReportType.PARKING;
+            else {
+                return new ResponseEntity<>("잘못된 reportType", HttpStatus.BAD_REQUEST);
+                log.error("::ERROR:: ReportController.java -> saveReport / 잘못된 reportType");
+            }
+            Report report;
             reportService.save(report);
             return ResponseEntity.ok("신고 접수가 완료되었습니다!");
         } catch (Exception e){
