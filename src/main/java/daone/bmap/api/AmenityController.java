1239e92e6562b08871c861d74018167f44ee963c
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-26 12:47:43 +0900

    Amenity API 개발 #4 #5 - 주차장 편의시설 데이터 더미데이터 자동 입력 API 구현

--- src/main/java/daone/bmap/api/AmenityController.java
+++ src/main/java/daone/bmap/api/AmenityController.java
@@ -12,10 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +34,17 @@ public class AmenityController {
         }catch (Exception e){
             log.error("::ERROR:: AmenityController.java -> findParkingDataByAmenity");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/save")
+    public ResponseEntity<?> saveAmenityData() {
+        try {
+            amenityService.injectDummyData();
+            return ResponseEntity.ok("장애인 편의 시설 데이터 저장 완료");
+        } catch (Exception e){
+            log.error("::ERROR:: AmenityController.java -> saveAmenityData");
+            return new ResponseEntity<>("장애인 편의 시설 데이터 저장 실패",HttpStatus.BAD_REQUEST);
         }
     }
 }
