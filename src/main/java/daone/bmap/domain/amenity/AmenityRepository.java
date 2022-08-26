d71cc53f2e0b6fbde983aecd99217cc87cd5d780
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-26 20:09:56 +0900

    Amenity API 개발 #4 - prkplceNo로 Amenity데이터 불러오기 구현

--- src/main/java/daone/bmap/domain/amenity/AmenityRepository.java
+++ src/main/java/daone/bmap/domain/amenity/AmenityRepository.java
@@ -1,11 +1,15 @@
 package daone.bmap.domain.amenity;
 
+import daone.bmap.domain.park.Park;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AmenityRepository extends JpaRepository<Amenity, Long> {
 
     Amenity save(Amenity amenity);
     List<Amenity> findByElevatorAndRampAndAccessRoadsAndWheelchairLiftAndBrailleBlockAndExGuidanceAndExTicketOffice(boolean elevator, boolean ramp, boolean accessRoads, boolean wheelchairLift, boolean brailleBlock, boolean exGuidance, boolean exTicketOffice);
+
+    Optional<Amenity> findByPark(Park park);
 }
