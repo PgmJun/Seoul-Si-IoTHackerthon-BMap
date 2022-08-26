cacd3d96ed368c735ddffdcae7a057c3404afb8c
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-26 23:19:51 +0900

    Bmap 개발 - autoCsvInstall 중단

--- src/main/java/daone/bmap/csv/autoCsvInstall.bat
+++ src/main/java/daone/bmap/csv/autoCsvInstall.bat
@@ -1,7 +1,7 @@
-rem D:
+D:
 
-rem cd \github\Seoul-Si-IoTHackerthon-BMap\src\main\java\daone\bmap\csv
+cd \github\Seoul-Si-IoTHackerthon-BMap\src\main\java\daone\bmap\csv
 
-rem curl https://www.data.go.kr/download/15012896/standard.do?dataType=csv > data.csv
+curl https://www.data.go.kr/download/15012896/standard.do?dataType=csv > data.csv
 
-rem exit
+exit
