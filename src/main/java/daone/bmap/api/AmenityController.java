21f2c28dbf5ef85575fca73c2df285073602a719
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-08-27 01:59:20 +0900

    Amenity API 개발 #4 - RequestDto 변수 추가 - Service method명 변경(findAmenityData -> findParkDataByAmenityData)

--- src/main/java/daone/bmap/api/AmenityController.java
+++ src/main/java/daone/bmap/api/AmenityController.java
@@ -1,25 +1,16 @@
 package daone.bmap.api;
 
-import daone.bmap.domain.amenity.Amenity;
-import daone.bmap.domain.amenity.AmenityRepository;
-import daone.bmap.domain.park.Park;
 import daone.bmap.dto.amenity.AmenityRequestDto;
 import daone.bmap.dto.amenity.AmenityResponseDto;
 import daone.bmap.dto.park.ParkDto;
-import daone.bmap.dto.park.ParkMapper;
 import daone.bmap.service.AmenityService;
-import daone.bmap.service.ParkService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.annotations.Parameter;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -31,7 +22,7 @@ public class AmenityController {
     @GetMapping("/find")
     public ResponseEntity<?> findParkingDataByAmenity(@RequestBody AmenityRequestDto data){
         try {
-            List<ParkDto> result = amenityService.findAmenityData(data);
+            List<ParkDto> result = amenityService.findParkDataByAmenityData(data);
             if(result.isEmpty())
                 return new ResponseEntity<>("데이터를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
             return ResponseEntity.ok(result);
