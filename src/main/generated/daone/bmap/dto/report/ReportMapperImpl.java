2b142f12e628edd20fde70429493d1d27fb5d0bb
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-09-10 23:28:20 +0900

    feat: API Swagger 설정 #10 - 전체 컨트롤러 API에 스웨거 적용

--- src/main/generated/daone/bmap/dto/report/ReportMapperImpl.java
+++ src/main/generated/daone/bmap/dto/report/ReportMapperImpl.java
@@ -7,8 +7,8 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-09-08T21:22:23+0900",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.16.1 (Oracle Corporation)"
+    date = "2022-09-10T23:18:23+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.11 (Oracle Corporation)"
 )
 public class ReportMapperImpl implements ReportMapper {
 
--- src/main/java/daone/bmap/api/ReportController.java
+++ src/main/java/daone/bmap/api/ReportController.java
@@ -1,13 +1,12 @@
 package daone.bmap.api;
 
-import daone.bmap.domain.park.Park;
 import daone.bmap.domain.report.Report;
 import daone.bmap.domain.report.ReportType;
-import daone.bmap.dto.report.ReportMapper;
 import daone.bmap.dto.report.ReportRequestDto;
 import daone.bmap.dto.report.ReportResponseDto;
 import daone.bmap.service.ParkService;
 import daone.bmap.service.ReportService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,8 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 import static daone.bmap.dto.report.ReportResponseDto.getReportResponseDto;
 
@@ -29,18 +26,21 @@ public class ReportController {
     private final ParkService parkService;
 
     @PostMapping("/add")
+    @ApiOperation(value = "신고 데이터 저장", notes = "입력받은 내용의 신고 데이터 저장")
     public ResponseEntity<String> saveReport(@RequestBody ReportRequestDto data) {
         reportService.save(new Report(parkService.findParkingLotByNo(data.getPrkplceNo()), ReportType.checkTypeCode(data.getReportType()), data.getReportTitle(), data.getReportText(), data.getReportCarNm()));
         return ResponseEntity.ok("신고 접수가 완료되었습니다!");
     }
 
     @GetMapping("/find/{reportId}")
+    @ApiOperation(value = "신고 데이터 검색(ID)", notes = "PK가 {reportId}인 신고 데이터 검색")
     public ResponseEntity<ReportResponseDto> findReportDataByPno(@PathVariable Long reportId) {
         return new ResponseEntity<>(getReportResponseDto(reportService.findReportByReportId(reportId)), HttpStatus.OK);
 
     }
 
     @GetMapping("/find/all")
+    @ApiOperation(value = "신고 데이터 검색(전체)", notes = "DB에 저장된 모든 신고 데이터 찾기")
     public ResponseEntity<?> findAllReportData() {
         List<ReportResponseDto> reportList = reportService.findReportAll();
         return (!reportList.isEmpty()) ?
