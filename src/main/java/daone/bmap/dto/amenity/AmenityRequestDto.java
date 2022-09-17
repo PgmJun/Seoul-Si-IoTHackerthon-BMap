cfde0d74d9aaea7bcb7707d9917c767af4fa9c2d
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-09-17 21:23:43 +0900

    refactor: if문 하드코딩 Map,Stream 사용하여 리팩터링 #9 - getExistAmenityList(): Boolean 갯수만큼 작성한 if문 Map,Stream 사용하여 리팩터링 - AmenityRequestDto: ObjectMapper 사용하여 toMap 메서드 구현

--- src/main/java/daone/bmap/dto/amenity/AmenityRequestDto.java
+++ src/main/java/daone/bmap/dto/amenity/AmenityRequestDto.java
@@ -1,13 +1,16 @@
 package daone.bmap.dto.amenity;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Getter
 @Setter
 public class AmenityRequestDto {
-
     private boolean elevator;   // 엘리베이터
     private boolean wideExit;   //넓은 출입구
     private boolean ramp;   // 경사로
@@ -19,4 +22,12 @@ public class AmenityRequestDto {
     private boolean wideRestroom;   // 장애인 전용 화장실
     private Double longitude;
     private Double latitude;
+
+    public Map<String, Boolean> toMap(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        HashMap result = objectMapper.convertValue(this, HashMap.class);
+        result.remove("longitude");
+        result.remove("latitude");
+        return result;
+    }
 }
